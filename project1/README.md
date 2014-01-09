82e7622dd6530f3235ce5b9c1b7109603dd4dafa
Yegor Pomortsev <yegor@pomortsev.com>
2014-01-08 16:09:46 -0800

    beatrepeat: add offset and shuffle

--- project1/README.md
+++ project1/README.md
@@ -14,25 +14,28 @@ Run `scons`
 ## Usage
 
     beatrepeat [options] infile outfile
+
     Options:
 
         -v    verbose. specify more to be more verbose
         -t    output file type/driver (see equivalent sox option)
         -T    tempo (decimal > 0)
         -b    beats (integer > 0)
+        -o    offset (integer >= 0)
         -c    max chunk slices (integer > 0)
         -p    max period slices (integer > 0)
+        -s    shuffle chance (decimal >= 0)
         -r    repeat chance (decimal >= 0)
         -S    random seed (integer)
 
 Example:
 
-    ./beatrepeat -T 170 -b 8 -c 8 -p 4 -r 0.3 -S 123456 -vv in.aif out.aif
+    ./beatrepeat -T 170 -b 8 -c 8 -p 4 -o 1 -s 0.2 -r 0.3 -S 123456 -vv in.aif out.aif
 
 To play the output instead of saving to file, set an output driver (exactly like SoX does it):
 
     # OS X
-    ./beatrepeat -t coreaudio -T 170 -b 8 -c 8 -p 4 -r 0.3 -S 123456 -vv in.aif default
+    ./beatrepeat -t coreaudio ... in.aif default
     
     # Linux (ALSA)
-    ./beatrepeat -t alsa -T 170 -b 8 -c 8 -p 4 -r 0.3 -S 123456 -vv in.aif default
+    ./beatrepeat -t alsa ... in.aif default
--- project1/beatrepeat.c
+++ project1/beatrepeat.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <time.h>
 #include <unistd.h>
 #include <inttypes.h>
 #include <string.h>
+#include <math.h>
 
 #include "sox.h"
 
 void usage() {
-    printf("beatrepeat [options] infile outfile\n"
+    printf("beatrepeat [options] infile outfile\n\n"
             "Options:\n\n"
             "    -v    verbose. specify more to be more verbose\n"
             "    -t    output file type/driver (see equivalent sox option)\n"
             "    -T    tempo (decimal > 0)\n"
             "    -b    beats (integer > 0)\n"
+            "    -o    offset (integer >= 0)\n"
             "    -c    max chunk slices (integer > 0)\n"
             "    -p    max period slices (integer > 0)\n"
+            "    -s    shuffle chance (decimal >= 0)\n"
             "    -r    repeat chance (decimal >= 0)\n"
-            "    -S    random seed (integer)");
+            "    -S    random seed (integer)\n");
 }
 
 typedef struct _arguments {
@@ -25,6 +29,8 @@ typedef struct _arguments {
     double beats;
     unsigned int chunk_slices;
     unsigned int period_slices;
+    unsigned int offset;
+    double shuffle_chance;
     double repeat_chance;
     unsigned int seed;
     char *output_type;
@@ -35,6 +41,10 @@ size_t bufsize(sox_format_t *format, double period) {
     buf_size = period * format->signal.rate * format->signal.channels + .5;
     buf_size -= buf_size % format->signal.channels;
     return buf_size;
+}
+
+double secs_per_beat(double tempo) {
+    return (1.0 / tempo) * 60;
 }
 
 int main(int argc, char *argv[]) {
@@ -49,6 +59,8 @@ int main(int argc, char *argv[]) {
     args.beats = 4;
     args.chunk_slices = 8;
     args.period_slices = 8;
+    args.offset = 2;
+    args.shuffle_chance = 0.3;
     args.repeat_chance = 0.75;
     args.seed = time(NULL);
 
@@ -59,7 +71,7 @@ int main(int argc, char *argv[]) {
 
     /* parse arguments */
     char c;
-    while ((c = getopt(argc, argv, "vt:T:b:c:p:r:S:")) != -1) {
+    while ((c = getopt(argc, argv, "vt:T:b:c:p:o:s:r:S:")) != -1) {
         switch (c) {
         case 'v': /* verbose */
             sox_get_globals()->verbosity++;
@@ -90,6 +102,18 @@ int main(int argc, char *argv[]) {
             break;
         case 'p': /* period slices */
             args.period_slices = strtoumax(optarg, NULL, 10);
+            break;
+        case 'o': /* offset */
+            args.offset = strtoumax(optarg, NULL, 10);
+            break;
+        case 's': /* shuffle chance */
+            args.shuffle_chance = strtod(optarg, NULL);
+
+            if (args.repeat_chance < 0) {
+                fprintf(stderr, "error: shuffle chance must be zero or more\n");
+                exit(EXIT_FAILURE);
+            }
+
             break;
         case 'r': /* repeat chance */
             args.repeat_chance = strtod(optarg, NULL);
@@ -124,38 +148,53 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    buf_period = (1.0 / args.tempo) * 60 * args.beats;
+    buf_period = secs_per_beat(args.tempo) * args.beats;
     buf_size = bufsize(in, buf_period);
+
+    unsigned int period_slices;
+    double buf_period_slice;
+    size_t buf_size_slice;
 
     buf = (sox_sample_t *) malloc(sizeof(sox_sample_t) * buf_size);
 
-    while ((read = sox_read(in, buf, buf_size)) > 0) {
-        unsigned int chunk_slices = (rand() % 2 == 0 ? 2 : 3) *
-            ((rand() % args.chunk_slices) + 1);
+    while ((read = sox_read(in, buf, buf_size)) != 0) {
+        period_slices = (2 * ((rand() % args.period_slices) + 1));
+        buf_period_slice = buf_period / period_slices;
+        buf_size_slice = bufsize(in, buf_period_slice);
 
-        int chunklen = (buf_size / in->signal.channels) / chunk_slices *
-            in->signal.channels;
+        double r;
+        for (unsigned int p = 0; p < period_slices; p++) {
+            r = rand() / (double) RAND_MAX;
 
-        unsigned int chunk;
-        unsigned int lastchunk = 0;
-        for (unsigned int i = 0; i < chunk_slices; i++) {
-            double r = rand() / (double) RAND_MAX;
-
-            if (r < args.repeat_chance) {
-                chunk = lastchunk;
+            unsigned int period_slice;
+            if (r < args.shuffle_chance) {
+                period_slice = rand() % period_slices;
             } else {
-                chunk = i;
-                lastchunk = chunk;
+                period_slice = p;
             }
 
-            sox_write(out, buf + chunk * chunklen, chunklen);
+            unsigned int chunk_slices = (rand() % 2 == 0 ? 2 : 3) *
+                ((rand() % (args.chunk_slices - 1)) + 1);
+
+            size_t chunklen = ((buf_size_slice / in->signal.channels) /
+                chunk_slices) * in->signal.channels;
+
+            unsigned int chunk = 0;
+            for (unsigned int c = 0; c < chunk_slices; c++) {
+                if (c > args.offset) {
+                    r = rand() / (double) RAND_MAX;
+                }
+
+                if (r > args.repeat_chance) {
+                    chunk = c;
+                }
+
+                sox_write(out,
+                        buf + (period_slice * buf_size_slice) +
+                            (chunk * chunklen), 
+                        chunklen);
+            }
         }
-
-        unsigned int period_slices = (2 * ((rand() % args.period_slices) + 1));
-
-        buf_period = (1.0 / args.tempo) * 60 * args.beats;
-        buf_period /= period_slices;
-        buf_size = bufsize(in, buf_period);
     }
 
     sox_close(out);
